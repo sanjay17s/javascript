878e737b2150add68357baa71a3831c0fa9c5f1a
sanjay17s <sanjay.17suresh2000@gmail.com>
2025-02-14 19:32:53 +0530

    objects

---
+++ basics/dataConvertions.js
@@ -0,0 +1,10 @@
+// let a =10
+// let toString = String(a)
+// console.log(toString)
+// console.log(typeof toString)
+// console.log(typeof (a + toString))
+
+let x = "12b3"
+let toNumber = Number(x)
+console.log(toNumber)
+console.log(typeof toNumber)
---
+++ basics/objects.js
@@ -0,0 +1,9 @@
+let names = ["himan", "dved"];
+
+let person = {
+    name: "guard",
+    age: 23
+};
+
+console.log(person.name); 
+console.log(person.age); 
